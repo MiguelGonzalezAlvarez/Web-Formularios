ec177e22e3284011058562d613ed9e2e787f47e3
MiguelMuros <miggonzalv@gmail.com>
2021-10-20 21:20:48 +0200

    feat: create side menu for routing

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { SideMenuComponent } from './side-menu/side-menu.component';
+
 
 
 
@@ -9,7 +11,8 @@ import { SideMenuComponent } from './side-menu/side-menu.component';
     SideMenuComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     SideMenuComponent
