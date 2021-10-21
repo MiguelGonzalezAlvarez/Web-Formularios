980c12c912f87d32bcfc27e373c5bcfea706ff60
MiguelMuros <miggonzalv@gmail.com>
2021-10-21 20:22:12 +0200

    feat: use ngModel to set initial values and reset form when submit

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -11,12 +11,23 @@ export class BasicosComponent implements OnInit {
 
   @ViewChild('miFormulario') miFormulario!: NgForm;
 
+  initForm = {
+    producto: 'Algo',
+    precio: 10,
+    existencias: 10
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
   guardar(): void {
+    this.miFormulario.resetForm({
+      producto: '',
+      precio: 0,
+      existencias: 0
+    });
   }
 
   nombreValido(): boolean {
@@ -27,7 +38,7 @@ export class BasicosComponent implements OnInit {
   }
 
   precioValido(): boolean {
-    const isNegative = this.miFormulario?.controls.precio?.value < 0 ;
+    const isNegative = this.miFormulario?.controls.precio?.value < 0;
     const touched = this.miFormulario?.controls.precio?.touched;
 
     return isNegative && touched;
