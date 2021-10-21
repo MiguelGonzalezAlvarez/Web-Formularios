c5cc2d50d2a6a2b1deb56810de461d35ac5c18ce
MiguelMuros <miggonzalv@gmail.com>
2021-10-21 20:51:09 +0200

    feat: add elements dynamically

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,16 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
+
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
-  styles: [
-  ]
+  styles: []
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
 
-  constructor() { }
+  persona: Persona = {
+    nombre: 'Miguel',
+    favoritos: [
+      { id: 1, nombre: 'Dark Souls' },
+      { id: 2, nombre: 'Bloodborne' },
+      { id: 3, nombre: 'Sekiro Shadows Die Twice' }
+    ]
+  }
 
-  ngOnInit(): void {
+  guardar(): void {
+
   }
 
 }
