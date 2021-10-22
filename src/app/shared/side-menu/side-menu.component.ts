76bc564d4bec0946bfbbb25f5cdff8694d9f733c
MiguelMuros <miggonzalv@gmail.com>
2021-10-22 07:58:49 +0200

    feat: create switchs page

--- src/app/shared/side-menu/side-menu.component.ts
+++ src/app/shared/side-menu/side-menu.component.ts
@@ -8,8 +8,7 @@ interface MenuItem {
 @Component({
   selector: 'app-side-menu',
   templateUrl: './side-menu.component.html',
-  styles: [
-  ]
+  styles: [`li { cursor: pointer }`]
 })
 export class SideMenuComponent {
 
