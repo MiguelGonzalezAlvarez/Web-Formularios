cd9069a57108697e84da53632e6c4e104a9edd5c
MiguelMuros <miggonzalv@gmail.com>
2021-10-26 19:31:32 +0200

    feat: use form group to create reactive forms

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -9,13 +9,39 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class BasicosComponent implements OnInit {
 
-  miFormulario: FormGroup = new FormGroup({
-    'nombre': new FormControl('RTX 4080')
-  });
+  // Este formato solo es valido para formularios pequeños
+  // miFormulario: FormGroup = new FormGroup({
+  //   nombre: new FormControl('RTX 4080'),
+  //   precio: new FormControl(1500),
+  //   existencias: new FormControl(5)
+  // });
 
-  constructor() { }
+  miFormulario: FormGroup = this.formBuilder.group({
+    nombre: ['', [Validators.required, Validators.minLength(3)]],
+    precio: [0, [Validators.required, Validators.min(0)]],
+    existencias: [0, [Validators.required, Validators.min(0)]]
+  })
 
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.miFormulario.reset({
+      nombre: 'RTX 4080',
+      precio: 1600
+    });
+  }
+
+  campoEsValido(campo: string) {
+    return !this.miFormulario.controls[campo].errors || !this.miFormulario.controls[campo].touched;
+  }
+
+  guardar(): void {
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+
+    this.miFormulario.reset();
   }
 
 }
