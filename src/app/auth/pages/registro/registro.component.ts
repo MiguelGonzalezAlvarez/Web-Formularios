85b73eeaaa4ac099f5342918802f7bdb4b3c442e
MiguelMuros <miggonzalv@gmail.com>
2021-10-27 22:12:11 +0200

    feat: create custom validation

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -11,12 +11,20 @@ export class RegistroComponent implements OnInit {
 
   nombreApellidoPattern: string = '([a-zA-Z]+) ([a-zA-Z]+) ([a-zA-Z]+)';
   emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+  validacionPersonalizada(control: FormControl) {
+    const valor: string = control.value?.trim().toLowerCase();
+    if (valor === 'miguelmuros') {
+      return { validacionPersonalizada: true }
+    }
+    return null;
+  }
 
   miFormulario: FormGroup = this.formBuilder.group({
     nombre: ['Miguel Gonzalez', [Validators.required, Validators.pattern(this.nombreApellidoPattern)]],
     email: ['', [Validators.required, Validators.pattern(this.emailPattern)]],
     // Forma alternativa estandar para validar que es email aunque yo utilizare un pattern personalizado más estricto
     // email: ['', [Validators.required, Validators.email]] 
+    username: ['', [Validators.required, this.validacionPersonalizada]]
 
   });
 
