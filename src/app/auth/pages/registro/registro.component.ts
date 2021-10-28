9b4593c9607c5bff1524dc450fdc0108c35a7f0e
MiguelMuros <miggonzalv@gmail.com>
2021-10-28 21:53:36 +0200

    feat: create email validator service and add more validations to the email

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,5 +1,7 @@
+import { getParseErrors } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EmailValidatorService } from 'src/app/shared/validator/email-validator.service';
 import { ValidatorService } from '../../../shared/validator/validator.service';
 
 @Component({
@@ -12,28 +14,46 @@ export class RegistroComponent implements OnInit {
 
   miFormulario: FormGroup = this.formBuilder.group({
     nombre: ['Miguel Gonzalez', [Validators.required, Validators.pattern(this.validatorService.nombreApellidoPattern)]],
-    email: ['', [Validators.required, Validators.pattern(this.validatorService.emailPattern)]],
+    email: ['', [Validators.required, Validators.pattern(this.validatorService.emailPattern)], [this.emailValidator]],
     // Forma alternativa estandar para validar que es email aunque yo utilizare un pattern personalizado más estricto
     // email: ['', [Validators.required, Validators.email]] 
     username: ['', [Validators.required, this.validatorService.validacionPersonalizada]],
     password: ['', [Validators.required, Validators.minLength(6)]],
     confirmacion: ['', [Validators.required]]
   }, {
-    validators: [this.validatorService.camposIguales('password','confirmacion')]
+    validators: [this.validatorService.camposIguales('password', 'confirmacion')]
   });
 
-  constructor(private formBuilder: FormBuilder, private validatorService: ValidatorService) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private validatorService: ValidatorService,
+    private emailValidator: EmailValidatorService
+  ) { }
 
   ngOnInit(): void {
     this.miFormulario.reset({
       nombre: 'Miguel Gonzalez Alvarez',
       email: 'miggonzalv@gmail.com',
       username: 'MiguelMuros',
+      password: 'password',
+      confirmacion: 'password'
     });
   }
 
   campoValido(campo: string) {
     return !(this.miFormulario.get(campo)?.invalid && this.miFormulario.get(campo)?.touched);
+  }
+
+  emailRequired() {
+    return this.miFormulario.get('email')?.errors?.required && this.miFormulario.get('email')?.touched;
+  }
+
+  emailFormato() {
+    return this.miFormulario.get('email')?.errors?.pattern && this.miFormulario.get('email')?.touched;
+  }
+
+  emailTomado() {
+    return this.miFormulario.get('email')?.errors?.emailTomado && this.miFormulario.get('email')?.touched;
   }
 
   submitForm() {
